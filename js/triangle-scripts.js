84365bba2a3964091eaf4fcc3c067c7faf0704e5
Ryan <ryanmccarthypdx@gmail.com>
2015-02-13 10:28:56 -0800

    Add three green specs

--- js/triangle-scripts.js
+++ js/triangle-scripts.js
@@ -8,7 +8,13 @@ var triangle = function(input) {
   var side3 = input.shift();
 
   if (side3 < (side2 + side1)) {
-
+    if ((side2 === side3) || (side2 === side1)) {
+      if(side3 === side1){
+        return "equilateral"
+      } else {
+        return "isosceles"
+      }
+    }
   } else {
     return false
   }
--- spec/triangle-specs.js
+++ spec/triangle-specs.js
@@ -1,6 +1,13 @@
 describe('triangle', function() {
-  it("returns 'false' if input is not a triangle", function() {
-    expect(triangle([1,1,12])).to.eql(false)
+  it("returns false if input is not a triangle", function() {
+    expect(triangle([1,1,12])).to.equal(false)
   });
 
+  it("returns 'equilateral' for equilateral triangle", function() {
+    expect(triangle([2,2,2])).to.equal("equilateral")
+  });
+
+  it("returns 'isosceles' for isosceles triangle", function() {
+    expect(triangle([2,3,2])).to.equal("isosceles")
+  });
 });
